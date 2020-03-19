b8c9780255e8c05b218ebd03f9a0b7a7136ba72f
ucfilho <42356381+ucfilho@users.noreply.github.com>
2020-03-19 15:45:39 -0300

    Update Function.py

--- Function.py
+++ Function.py
@@ -1,12 +1,212 @@
 #from AvaliaX import AvaliaX
 #from Go2Ann import Go2Ann
-import Area
-import buildMatriz
-import CustoProcesso
-import Temperatura
-import Utilidades
+def Area(x):
 
+  global mCp,tempAlvo,tempInicial,Trocas,U
+  
+  NPAR=1
+  
+  Area=[]
 
+  Ind_I,Ind_J,deltaT,Tin, Tout=buildMatriz(x) 
+  Tatual=np.copy(np.array(tempInicial))
+
+  rows=Trocas 
+  cols=len(mCp)
+
+  Area_Particula=[]
+
+  for k in range(Trocas):
+
+    i=Ind_I[0,k]
+    j=Ind_J[0,k]
+    tempSaidaQ=Tatual[i]+deltaT[0,k]
+    tempSaidaF=-deltaT[0,k]*mCp[i]/(mCp[j])+Tatual[j]
+
+    Told_i=Tatual[i]
+    Told_j=Tatual[j]
+
+    Tatual[i]=np.copy(tempSaidaQ)
+    Tatual[j]=np.copy(tempSaidaF)
+    deltaT1=Tatual[i]-tempSaidaF
+    deltaT2=tempSaidaQ-Tatual[j]
+    try:
+      A=(deltaT1-deltaT2)
+      B=np.log((deltaT1/deltaT2))
+      deltaTMediaLog=A/B
+    except:
+      deltaTMediaLog=1e99
+  
+    if(deltaTMediaLog==1e99):
+      AreaCalc=1e99
+    else:
+      AreaCalc=abs(deltaT[0,k]*mCp[i]/(U*deltaTMediaLog))
+
+    if(Told_i==Tatual[i]):
+      AreaCalc=0
+
+    if(Told_j==Tatual[j]):
+      AreaCalc=0
+
+      Area_Particula.append(AreaCalc)
+      
+  Area.append(Area_Particula)
+
+  return Area
+
+def buildMatriz(x):
+  global Trocas, tempInicial, tempAtual 
+  NPAR=1
+  cols=len(tempInicial)
+  size=3*cols
+  x=np.array(x).reshape(1,size)
+  # print('x=');print(x)
+  Ind_I=np.zeros((NPAR,Trocas)).astype(np.int64)
+  Ind_J=np.zeros((NPAR,Trocas)).astype(np.int64)
+  Tin=np.zeros((NPAR,Trocas))
+  Tout=np.zeros((NPAR,Trocas))
+  deltaT=np.zeros((NPAR,Trocas))
+  for i in range(NPAR):
+    for j in range(Trocas):
+      #print('[i,j]=%d %d'%(i,j))
+      Ind_I[i,j]=round(x[i,j])
+      Ind_J[i,j]=round(x[i,j+Trocas])
+      deltaT[i,j]=x[i,j+2*Trocas] 
+    for jj in range(cols):
+      Tin[i,jj]=tempInicial[jj]
+      Tout[i,jj]=tempAtual[jj]
+  return Ind_I,Ind_J,deltaT, Tin, Tout
+
+def CustoProcesso(Area,Utilidades,cargaUFtodas,cargaUQtodas):
+  global coefCustoUQ,coefCustoUF
+  
+  NPAR=1
+
+  cost=np.zeros((NPAR))
+  for i in range(NPAR):
+    cargaUQ=cargaUQtodas[i]
+    cargaUF=cargaUFtodas[i]
+    areaTrocadores=Area[i]
+    areaUtilidades=Utilidades[i]
+    costTrocadores=0
+    for it in areaTrocadores:
+      for wii in areaUtilidades:
+        if(it<1e99):
+          if(wii<1e99):
+            costTrocadores=costTrocadores+300*(it**0.5+wii**0.5)
+          else:
+            costTrocadores=1e99
+            break
+    costUQ=coefCustoUQ*cargaUQ
+    costUF=coefCustoUF*cargaUF
+    
+    if(costTrocadores==1e99):
+      cost[i]=1e99
+    else:
+      cost[i]=costUQ+costUF+costTrocadores
+      
+  return cost
+
+def Temperatura(x):
+  global Trocas, mCp,tempInicial
+  
+  NPAR=1
+  Ind_I,Ind_J,deltaT,Tin, Tout=buildMatriz(x) 
+  Tatual=np.copy(np.array(tempInicial))
+
+  erro=np.zeros((NPAR)) # valor erro se torna erro=1 quando ha algum erro e vai penalizar
+  for ki in range(NPAR):
+    for kj in range(Trocas):
+      #Tin=np.copy(Tout)
+      k=kj
+      i=Ind_I[ki,kj]
+      j=Ind_J[ki,kj]
+      
+      tempSaidaQ=Tout[ki,i]+deltaT[ki,kj]
+      tempSaidaF=-mCp[kj]*deltaT[ki,kj]/(mCp[j])+Tout[ki,j]
+      flag=0 # flag : indica que nao entra no teste logico
+      # usei flag no lugar q ifs pq a identacao ficou melhor assim
+      if(Tout[ki,i] > Tout[ki,j]): #Tquente maior Tfria: vaores atuais
+        if(tempSaidaQ > tempSaidaF): #Tquente maior Tfria: saidas
+          flag=1
+      if(flag>0):
+        Tout[ki,i]=tempSaidaQ
+        Tout[ki,j]=tempSaidaF
+      else:
+        erro[ki]=1
+
+  return Tout,erro
+
+def Utilidades(tempAlvo,Tout,mCp):
+
+  cols=len(mCp)
+  NPAR=1
+
+  
+  '''
+  caso I cita TUQin=553 e TQout=552, TUFin=303 TUFout=353
+  '''
+  
+  TUQin=553
+  TUQout=552
+  TUFin=303 
+  TUFout=353
+  
+  Areas=[]
+  cargaUQtodas=[]
+  cargaUFtodas=[]
+
+  for ki in range(NPAR):
+    areaTrocador=[]
+    cargaUQ=0
+    cargaUF=0
+    for j in range(cols):
+      delta=tempAlvo[j]-Tout[ki,j]
+      if(delta>0):#usa UQ
+        cargaUQ=cargaUQ+mCp[j]*delta
+        deltaT1=(TUQin-tempAlvo[j])
+        deltaT2=(TUQout-Tout[ki,j])
+
+        try:
+          A=deltaT1-deltaT2
+          B=np.log(deltaT1/deltaT2)
+          deltaTMediaLog=A/B
+        except:
+          deltaTMediaLog=1e99
+
+        if(deltaTMediaLog==1e99):
+          areaTrocador.append(1e99)
+        else:
+          areaTrocador.append((abs(mCp[j]*(delta)/(U*deltaTMediaLog))))
+
+      elif(delta==0):#nao usa UF e UQ
+        deltaTMediaLog=0
+        areaTrocador.append(0) 
+
+      else:#usa UF
+        deltaT1=(Tout[ki,j]-TUFout)
+        deltaT2=(tempAlvo[j]-TUFin)     
+        cargaUF=cargaUF+mCp[j]*(-delta)
+
+        #UF -> água resfriada -> entra a 303K e sai a 353K
+        try:
+          A=deltaT1-deltaT2
+          B=np.log(deltaT1/deltaT2)
+          deltaTMediaLog=A/B
+        except:
+          deltaTMediaLog=1e99
+        
+        if(deltaTMediaLog==1e99):
+          areaTrocador.append(1e99)
+        else:
+          areaTrocador.append((abs(mCp[j]*(-delta)/(U*deltaTMediaLog))))
+
+    cargaUQtodas.append(cargaUQ)
+    cargaUFtodas.append(cargaUF)
+
+    Areas.append(areaTrocador)
+
+  return Areas, cargaUQtodas,cargaUFtodas 
 
 def Function(x):
 
